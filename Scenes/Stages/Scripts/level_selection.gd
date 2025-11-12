6dbfd942b79c991593a92e0d4c2859e3df5ecff3
Gunturadhtya <gunturpdng@gmail.com>
2025-11-13 04:04:57 +0800

    Add CedeEnemy to stage 3 and implement stage selection
    Added a new CedeEnemy instance to stage_3.tscn and set its position and properties. Updated level_selection.gd to enable navigation to stages 1, 2, and 3 when their respective buttons are pressed.

--- Scenes/Stages/Scripts/level_selection.gd
+++ Scenes/Stages/Scripts/level_selection.gd
@@ -17,12 +17,12 @@ func _on_stage_3_pressed() -> void:
 
 
 func _on_stage_4_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Stages/1_Stage/stage_1.tscn")
 
 
 func _on_stage_5_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Stages/2_Stage/stage_2.tscn")
 
 
 func _on_stage_6_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Stages/3_Stage/stage_3.tscn")
